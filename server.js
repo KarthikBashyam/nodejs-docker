0411985c679829c2cb1cd2eb3f4ba06f3d5db774
Karthik <karthik@email.com>
2019-05-25 14:11:22 -0400

    Added post method

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
-let app = express()
+let app = express();
 
 app.set('port', 3000);
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-    res.send('Nodejs Docker');
+    res.send('Hello World Nodejs Docker');
 })
+
+app.post('/event', (req, res) => {	
+	console.log('EVENT RECEIVED :'+ req.body);
+	res.json('Ok');
+});
 
 app.listen(app.get('port'), () => {
     console.log('Express server started on port :' + app.get('port'));
